e888155343ad86d1210db6483d21f97af258d9a6
ABDO-BST <dmzab.47@gmail.com>
2021-11-16 23:12:03 +0100

    first commit

---
+++ src/pages/Create.js
@@ -0,0 +1,12 @@
+const Create = () => {
+    return ( 
+
+
+         <div className="notes">
+            <h1>hello worls this is the create page !!!!</h1>
+        </div>
+     );
+}
+ 
+export default Create;
+
---
+++ src/pages/Notes.js
@@ -0,0 +1,12 @@
+const Notes = () => {
+    return ( 
+
+        <div className="notes">
+            <h1>hello worls this is the notes page !!!!</h1>
+        </div>
+
+        
+     );
+}
+ 
+export default Notes;
